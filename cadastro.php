ceedec7d807544909b64d2c3e452ae6ac37c4d14
rafael7485637 <142898542+rafael7485637@users.noreply.github.com>
2025-06-20 00:45:10 +0000

    4

--- cadastro.php
+++ cadastro.php
@@ -61,24 +61,24 @@
     </header>
     <main>
 
-    <div class="container-lg mt-4">
-      <form>
-        <div class="mb-3">
-          <label for="nome" class="form-label">Nome</label>
-          <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        
-
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="cadastro container-lg mt-4">
+      <form action="">
+          <div>
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" />
+          </div>
+          <div>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" />
+          </div>
+          <div>
+            <label for="telefone">Telefone:</label>
+            <input type="number" id="Telefone" />
+          </div>
+          <div>
+            <label for="idade">Idade:</label>
+            <input type="number" id="idade" />
+          </div>
       </form>
     </div>
   
