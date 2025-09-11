e9c650300c785c3b3bab95f1bae121419f5fb958
rafael7485637 <142898542+rafael7485637@users.noreply.github.com>
2025-09-11 19:57:24 +0000

    11

--- script.js
+++ script.js
@@ -61,3 +61,38 @@ document.getElementById("prev").addEventListener("click", () => {
 
 // inicia o carrossel
 updateCarousel();
+
+// Atualiza o carrossel com animação suave
+function updateCarousel() {
+  // Atualiza as 5 imagens
+  cardEls[0].src = images[(index - 2 + images.length) % images.length];
+  cardEls[1].src = images[(index - 1 + images.length) % images.length];
+  cardEls[2].src = images[index % images.length];
+  cardEls[3].src = images[(index + 1) % images.length];
+  cardEls[4].src = images[(index + 2) % images.length];
+
+  // Define as classes de profundidade para os cards
+  cards.forEach((card, i) => {
+    card.classList.remove("far", "near", "active");
+
+    // Adiciona as classes de profundidade dependendo da posição
+    if (i === 0 || i === 4) card.classList.add("far");    // mais distante
+    else if (i === 1 || i === 3) card.classList.add("near"); // lateral próxima
+    else if (i === 2) card.classList.add("active");       // central
+  });
+}
+
+// Eventos de navegação
+document.getElementById("next").addEventListener("click", () => {
+  index = (index + 1) % images.length;
+  updateCarousel();
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+  index = (index - 1 + images.length) % images.length;
+  updateCarousel();
+});
+
+// Inicia o carrossel
+updateCarousel();
+
